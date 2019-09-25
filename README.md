b2bf3ef004c2b0fbd992e329a27e53a91d769c7d
alpersonalwebsite <mypersonalwebsite@outlook.com>
2019-09-25 09:00:46 -0700

    First commit

--- README.md
+++ README.md
@@ -1,68 +1,36 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Basic TODO board
 
-## Available Scripts
+[![Greenkeeper badge](https://badges.greenkeeper.io/alpersonalwebsite/basic-todo-board.svg)](https://greenkeeper.io/)
+[![License: MIT](https://img.shields.io/badge/License-MIT-brightgreen.svg)](https://opensource.org/licenses/MIT)
 
-In the project directory, you can run:
+This is an easy, basic and raw (minimum styling) example of **HOW to** create a `TODO board`
 
-### `npm start`
+**NOTE:** This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Installation
+```
+yarn install
+```
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+## Running the dev server
+```
+yarn start
+```
 
-### `npm test`
+## Description
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+...
 
-### `npm run build`
+Using `React.memo()` 
 
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
+The first time we render our App we should see in our browser's `console` something like this:
 
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
+![First render](images/first.png)
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+However, what happens if we remove a task...?
 
-### `npm run eject`
+![After removing a task](images/removing-task.png)
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+You can see that everything renders again, but that particular task. 
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+In this small application we will not suffer "performance issues", yet, we can improve its efficiency. 
--- src/App.js
+++ src/App.js
@@ -1,26 +1,181 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
+class App extends Component {
+
+ state = {
+  toDo: [
+    { id: '1', title: 'Title 1...', description: 'Description 1...' },
+    { id: '2', title: 'Title 2...', description: 'Description 2...' },
+    { id: '3', title: 'Title 3...', description: 'Description 3...' }
+  ],
+  inProgress: [
+    { id: '4', title: 'Title 4...', description: 'Description 4...' },
+    { id: '5', title: 'Title 5...', description: 'Description 5...' }
+  ],
+  done: [
+    { id: '6', title: 'Title 6...', description: 'Description 6...' }
+  ],
+  title: '',
+  description: '',
+  type: 'toDo'
+ }
+
+ // Since this never changes, we are going to set it as a property of our class and not part of the state
+listOfStatus = {
+   toDo: 'To Do',
+   inProgress: 'In Progress',
+   done: 'Done'
+ };
+
+// List
+listOfStatusKeys = Object.keys(this.listOfStatus)
+
+
+ // Control inputs
+  changeHandler = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+   // Add by default to To Do but user can change it
+   addTaskHandler = (event) => {
+     event.preventDefault();
+     let newTask = {};
+     newTask.id = Date.now();
+     newTask.title = this.state.title;
+     newTask.description = this.state.description;
+
+     let category = this.state.type
+
+     this.setState({
+      [category]: [...this.state[category], newTask],
+       title: '',
+       description: ''
+     })
+  }
+  
+  // Remove
+  removeTaskHandler = (taskId, taskType) => {
+    let newStatusState = [...this.state[taskType]];
+    newStatusState = newStatusState.filter(element => element.id !== taskId) 
+    this.setState({
+      [taskType]: newStatusState
+    })
+  }
+
+  // Move from one to other column (first and last should not move)
+  moveTask = (event, currentColumn, prevOrNext) => {
+    event.preventDefault();
+    console.log(event.target, currentColumn, prevOrNext)
+  }
+
+
+ render() {
+   console.log('APP')
+  let listOfCategories = null;
+  if (this.listOfStatus) {
+    listOfCategories = this.listOfStatusKeys.map((element, index) => {
+      return <ListOfTasks key={element} type={element}
+      tasks={this.state[element]} onClickRemove={this.removeTaskHandler}
+      onMoveTask={this.moveTask} listOfStatus={this.listOfStatus}>
+      
+      {this.listOfStatus[element]}
+      </ListOfTasks>
+    })
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List</h1>
+      <div className="container">
+        {listOfCategories}
+      </div>
+      <h1>Add Task</h1>
+      <div className="container"> 
+        <AddTask onSubmit={this.addTaskHandler} 
+        onInputChange={this.changeHandler} 
+        type={this.state.type}
+        title={this.state.title}
+        description={this.state.description}
+        listOfStatus={this.listOfStatus}  
+        />
+      </div>
     </div>
   );
+ }
 }
+
+const ListOfTasks = props => {
+  console.log('LISTOFTASKS')
+  return (
+    <div className="column border padding25">
+      <h3>{props.children}</h3>
+        {
+          props.tasks.map((element, index) => {
+            return <Task key={index} type={props.type}
+            onClickRemove={props.onClickRemove}
+            onMoveTask={props.onMoveTask}
+            >{element}</Task>
+          })
+        }
+    </div>
+  )
+}
+
+
+const Task = props => {
+  console.log('TASK')
+  return (
+    <div style={{ marginBottom: 10 }}>
+      <div>{props.children.title}</div>
+      <div>{props.children.description} 
+        <button style={{ cursor: 'pointer' }} 
+          onClick={() => props.onClickRemove(props.children.id, props.type)}>[Remove me]
+        </button>
+        <div>
+          <button onClick={(event) => props.onMoveTask(event, 'currentColumn', 'prevOrNext')}
+          name={props.type}
+          >Move</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const AddTask = props => {
+
+  const listOfStatusKeys = Object.keys(props.listOfStatus)
+
+  return (
+    <div>
+      <form onSubmit={props.onSubmit}>
+        <div>
+          <label>Type:</label>
+          <select name='type' onChange={props.onInputChange} value={props.type} >
+            {
+              listOfStatusKeys.map(element => {
+                return <option key={element} value={element}>{props.listOfStatus[element]}</option>
+              })
+            }
+          </select>
+        </div>
+        <div>
+          <label>Title:</label><input type='text' name='title' 
+          onChange={props.onInputChange} value={props.title} />
+        </div>
+        <div>
+          <label>Description:</label><input type='text' name='description' 
+          onChange={props.onInputChange} value={props.description}  />
+        </div>
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
 
 export default App;
